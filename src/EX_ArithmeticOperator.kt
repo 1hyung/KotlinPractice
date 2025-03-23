aecaaa7e47230b9be3ce85bfb4d7bc7369485edd
1hyung <ryu1hyung@naver.com>
2025-03-24 00:53:56 +0900

    :pencil2: Study: Learn Arithmetic Operator in Kotlin

---
+++ src/EX_ArithmeticOperator.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val num1: Int = 15 - 4 * 3
+    val num2: Int = 65 % 7
+    val num3: Double = 7.5 / 5 + 22.50
+    val num4: Double = num1 / num2 + 0.7
+
+    println(num1)
+    println(num2)
+    println(num3)
+    println(num4)
+}
