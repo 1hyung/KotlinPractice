bf8a20b306933f5b327b3bc0f4cc0dfb62f78205
ryuwonhyung <dev.1hyung@gmail.com>
2025-06-13 11:29:58 +0900

    :pencil2: Study: 컬렉션 함수 flatMap 학습

---
+++ src/collectionFunctions/EX_flatMap.kt
@@ -0,0 +1,18 @@
+package collectionFunctions
+
+data class Order(val id: Int, val productNames: List<String>)
+
+val orders = listOf(
+    Order(1, listOf("딸기", "레몬", "딸기")),
+    Order(2, listOf("사과", "체리", "포도")),
+    Order(3, listOf("수박", "메론", "레몬"))
+)
+
+fun main() {
+    // 모든 주문의 상품 이름을 하나의 리스트로 합침
+    val allProductNames = orders.flatMap { order ->
+        order.productNames // 각 주문에서 상품 이름 리스트를 반환
+    }
+    println("모든 상품 이름 : $allProductNames") // 같은 아이디나 다른 아이디에서 같은 주문을 해도 합쳐서 출력됨
+    // 출력 : [딸기, 레몬, 딸기, 사과, 체리, 포도, 수박, 메론, 레몬]
+}
