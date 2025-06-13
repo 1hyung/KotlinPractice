5fef9510460bdcaa8743ebfd77b9d2403bf81917
ryuwonhyung <dev.1hyung@gmail.com>
2025-06-13 16:53:25 +0900

    :pencil2: Study: 컬렉션함수 firstOrNull 학습

---
+++ src/collectionFunctions/EX_firstOrNull.kt
@@ -0,0 +1,24 @@
+package collectionFunctions
+
+data class Ord (val name: String, val type: String)
+
+val ords = listOf(
+    Ord("루피", "물딜"),
+    Ord("조로", "물딜"),
+    Ord("나미", "마딜")
+)
+
+fun main () {
+    // 타입이 마딜인 캐릭을 찾음
+    val firstMagicOrd = ords.firstOrNull { ord -> ord.type == "마딜" }
+    println(firstMagicOrd?.name)
+
+    val firstMuliOrd = ords.firstOrNull { it.type == "물딜" }
+    println(firstMuliOrd?.name)
+
+    // null이 아닐 때 출력하고싶다
+    ords.firstOrNull { it.type == "듀얼" }?.also { firstDualOrd ->
+        println(firstDualOrd.name)
+    }
+
+}
