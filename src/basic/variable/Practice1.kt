33a5837737fb4ffdc47580171a4406c5a8d3142d
1hyung <dev.1hyung@gmail.com>
2025-11-20 23:36:18 +0900

    :pencil2: Study: 변수 선언 및 타입 추론 학습

--- src/basic/variable/Practice1.kt
+++ src/basic/variable/Practice1.kt
@@ -1,22 +1,22 @@
 package basic.variable
 
-val name: String = "원형"
-var age: Int = 30
-var email: String = "RYU@example.com"
-
 fun main() {
     // 1. 자기소개 하기
+    val name = "원형"
+    var age = 30
+    val email = "RYU@example.com"
+
     age = 31
 
-    println("이름은 ${name}이고, 나이는 ${age}이고, 이메일은 ${email}입니다")
+    println("이름은 $name 이고, 나이는 $age 이고, 이메일은 $email 입니다")
 
-    val pr: String = "이름은 ${name}이고, 나이는 ${age}이고, 이메일은 ${email}입니다"
+    val pr = "이름은 $name 이고, 나이는 $age 이고, 이메일은 $email 입니다"
     println(pr)
 
     // 2. 적절한 변수 타입 넣기
-    val age: Int = 10
-    val pi: Double = 3.1415
-    val isPossible: Boolean = true
-    val constructionCosts: Long = 5_000_000_000
-    val name: String = "원형"
+    val childAge = 10
+    val pi = 3.1415
+    val isPossible = true
+    val constructionCosts = 5_000_000_000L
+    val anotherName = "행도"
 }
